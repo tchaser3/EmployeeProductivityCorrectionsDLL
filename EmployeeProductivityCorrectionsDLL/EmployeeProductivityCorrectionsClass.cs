5902155d27ddeb40199237b0663f1d06c61265df
Terry Holmes <tchaser3@outlook.com>
2024-03-23 10:43:37 -0400

    Add project files.

---
+++ EmployeeProductivityCorrectionsDLL/EmployeeProductivityCorrectionsClass.cs
@@ -0,0 +1,23 @@
+﻿/* Title:           Employee Productivity Corrections Class
+ * Date:            1-22-20
+ * Author:          Terry Holmes
+ * 
+ * Descroiption:    This is used to show problems and corrections with employee productivity */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewEventLogDLL;
+
+namespace EmployeeProductivityCorrectionsDLL
+{
+    public class EmployeeProductivityCorrectionsClass
+    {
+        //setting up the classes
+        EventLogClass TheEventLogClass = new EventLogClass();
+
+
+    }
+}
